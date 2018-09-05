25fc087f2e3bd9157d0e15a0466bfc1d0658838f
sovovy <sovovy@gmail.com>
2018-09-05 12:45:31 +0900

    DetailActivity 추가. 간단한 구성. 컬러 및 스타일 추가.

---
+++ app/src/main/java/com/hyeong/handsomego/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.hyeong.handsomego.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hyeong.handsomego.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
