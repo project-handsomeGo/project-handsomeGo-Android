7dadf76cc3fea27cb590458b56f0111371fffbf6
Daeun <daeuni>
2018-09-05 10:04:26 +0900

    ZoomView Setting : Installing Libraries, ZoomView XML

--- app/src/main/java/com/hyeong/handsomego/HomeTabFragment.kt
+++ app/src/main/java/com/hyeong/handsomego/HomeTabFragment.kt
@@ -1,17 +1,26 @@
 package com.hyeong.handsomego
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import pl.polidea.view.ZoomView
 
-/**
- * Created by HYEON on 2018-09-03.
- */
+
 class HomeTabFragment : Fragment() {
+
+    var v : View? = null
+    var zoomView: ZoomView? = null
+
+    //private var networkService : NetworkService? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_hometab,container,false)
+        val view = (context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE)as LayoutInflater).inflate(R.layout.fragment_hometab_zoomview,null,false)
+
+
         return v
 
     }
