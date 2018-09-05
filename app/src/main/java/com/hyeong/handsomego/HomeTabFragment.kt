7a4339bc190a43a333681db485a61099c7f226b0
Daeun <daeuni>
2018-09-05 10:13:13 +0900

    Adding Zoomview on MapContainer

--- app/src/main/java/com/hyeong/handsomego/HomeTabFragment.kt
+++ app/src/main/java/com/hyeong/handsomego/HomeTabFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import pl.polidea.view.ZoomView
 
 
@@ -19,7 +21,19 @@ class HomeTabFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_hometab,container,false)
         val view = (context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE)as LayoutInflater).inflate(R.layout.fragment_hometab_zoomview,null,false)
+        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
 
+        zoomView = ZoomView(context)
+        zoomView!!.addView(view)
+        zoomView!!.layoutParams = layoutParams
+
+        zoomView!!.isMiniMapEnabled = true // 좌측 상단 검은색 미니맵 설정
+        zoomView!!.maxZoom = 4f
+        zoomView!!.miniMapCaption = "Mini Map Test" // 미니맵 내용
+        zoomView!!.miniMapCaptionSize = 20f // 미니맵 내용 글씨 크기 설정
+
+        val container = v!!.findViewById<RelativeLayout>(R.id.mapContainer)
+        container.addView(zoomView)
 
         return v
 
