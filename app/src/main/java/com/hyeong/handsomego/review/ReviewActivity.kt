dfeb1a793a51876f773fe2049c6b48a3e39f6253
sovovy <sovovy@gmail.com>
2018-09-12 17:44:38 +0900

    <리뷰하기> 작성 중

---
+++ app/src/main/java/com/hyeong/handsomego/review/ReviewActivity.kt
@@ -0,0 +1,13 @@
+package com.hyeong.handsomego.review
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hyeong.handsomego.R
+
+class ReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+    }
+}
