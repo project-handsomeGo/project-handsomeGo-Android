702402e3309f907c86b18e82790bdeecef7028b2
sovovy <sovovy@gmail.com>
2018-09-19 21:32:49 +0900

    리뷰 별점 누르기 가능

--- app/src/main/java/com/hyeong/handsomego/review/ReviewActivity.kt
+++ app/src/main/java/com/hyeong/handsomego/review/ReviewActivity.kt
@@ -2,12 +2,17 @@ package com.hyeong.handsomego.review
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
 import com.hyeong.handsomego.R
+import kotlinx.android.synthetic.main.activity_review.*
 
-class ReviewActivity : AppCompatActivity() {
+class ReviewActivity : AppCompatActivity(), RatingBar.OnRatingBarChangeListener {
+    override fun onRatingChanged(ratingBar: RatingBar?, rating: Float, fromUser: Boolean) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+        reviewing_rate_rating.onRatingBarChangeListener = this
     }
 }
