315de685876fe3a57c2fa6bb3db8f78710b5bab8
sovovy <sovovy@gmail.com>
2018-09-20 17:32:19 +0900

    <스탬프 찍으면 나오는 간단 설명> 뷰 완성

---
+++ app/src/main/java/com/hyeong/handsomego/after_stamp/SimpleInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.hyeong.handsomego.after_stamp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hyeong.handsomego.R
+
+class SimpleInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_info)
+    }
+}
