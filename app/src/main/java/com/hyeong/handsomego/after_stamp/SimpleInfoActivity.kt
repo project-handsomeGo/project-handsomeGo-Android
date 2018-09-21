b3ca2c804f6284306122625a72b8a3e203b72250
sovovy <sovovy@gmail.com>
2018-09-21 19:04:58 +0900

    <스탬프 찍고 나서 나오는 뷰> 수정. 이미지 추가.

--- app/src/main/java/com/hyeong/handsomego/after_stamp/SimpleInfoActivity.kt
+++ app/src/main/java/com/hyeong/handsomego/after_stamp/SimpleInfoActivity.kt
@@ -1,12 +1,17 @@
 package com.hyeong.handsomego.after_stamp
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.hyeong.handsomego.R
 
 class SimpleInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_info)
+        // Full Screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
