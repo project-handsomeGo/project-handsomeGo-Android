edd51c05e0bcbcef3b40ceec599514b5ac728cd3
sovovy <sovovy@gmail.com>
2018-09-21 20:02:33 +0900

    gradle에 Glide 추가. <스탬프 찍고 나오는 간단 설명> 통신 중.

--- app/src/main/java/com/hyeong/handsomego/after_stamp/SimpleInfoActivity.kt
+++ app/src/main/java/com/hyeong/handsomego/after_stamp/SimpleInfoActivity.kt
@@ -4,14 +4,41 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.bumptech.glide.Glide
 import com.hyeong.handsomego.R
+import com.hyeong.handsomego.applicationController.ApplicationController
+import com.hyeong.handsomego.applicationController.NetworkService
+import com.hyeong.handsomego.get.GetPlaceInfoResponse
+import kotlinx.android.synthetic.main.activity_simple_info.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SimpleInfoActivity : AppCompatActivity() {
+    var networkService : NetworkService= ApplicationController.instance.networkService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_info)
         // Full Screen
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         window.statusBarColor = Color.TRANSPARENT
+
+        // Get Place Info
+        val getPlaceInfoResponse = networkService.getPlaceInfo(1)
+        getPlaceInfoResponse.enqueue(object : Callback<GetPlaceInfoResponse>{
+            override fun onFailure(call: Call<GetPlaceInfoResponse>?, t: Throwable?) {
+            }
+
+            override fun onResponse(call: Call<GetPlaceInfoResponse>?, response: Response<GetPlaceInfoResponse>?) {
+                if(response!!.isSuccessful){
+                    simple_name_tv.text = response.body().data[0].place_name
+                    simple_address_tv.text = response.body().data[0].place_address
+                    simple_info_tv.text = response.body().data[0].place_content
+                    simple_review_tv.text = response.body().data[0].commentCount.toString()
+                    Glide.with(SimpleInfoActivity()).load(response.body().data[0].place_pic).into(simple_img_iv)
+                }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/hyeong/handsomego/applicationController/NetworkService.kt
+++ app/src/main/java/com/hyeong/handsomego/applicationController/NetworkService.kt
@@ -1,15 +1,20 @@
 package com.hyeong.handsomego.applicationController
 
 
+import com.hyeong.handsomego.get.GetPlaceInfoResponse
 import com.hyeong.handsomego.get.GetStampInfoResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 /**
  * Created by HYEON on 2018-09-19.
  */
 interface NetworkService {
+
+    @GET("places/{place_id}")
+    fun getPlaceInfo(@Path("place_id") idx : Int):Call<GetPlaceInfoResponse>
 
     //마이페이지 조회
     //@GET()
